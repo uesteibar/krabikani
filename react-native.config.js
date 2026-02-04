bc5582e0e63e5b082d024b69b715a5ccf26e9af1
uesteibar <uesteibar@gmail.com>
2026-02-04 14:50:07 +0100

    Fix build

--- react-native.config.js
+++ react-native.config.js
@@ -4,4 +4,20 @@ module.exports = {
     android: {},
   },
   assets: ['./assets/fonts/'],
+  dependencies: {
+    '@react-native-vector-icons/material-design-icons': {
+      platforms: {
+        android: {
+          cmakeListsPath: null,
+        },
+      },
+    },
+    'react-native-keychain': {
+      platforms: {
+        android: {
+          cmakeListsPath: null,
+        },
+      },
+    },
+  },
 };
